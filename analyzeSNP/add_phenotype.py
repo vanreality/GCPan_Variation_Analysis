7fbc6616f435f8903adf643e7b5de0b448b0449e
fan <vanshiyu@gmail.com>
2021-06-16 18:05:07 +0800

    Add scripts for germline GWAS

---
+++ analyzeSNP/add_phenotype.py
@@ -0,0 +1,23 @@
+import argparse
+import pandas as pd
+
+
+def main(args):
+    phenotype = pd.read_table(args.p)
+    fam = pd.read_table(args.f, header=None)
+
+    for index, row in phenotype.iterrows():
+        fam.loc[fam[1] == row["Normal"], 4] = row["Gender"]
+        fam.loc[fam[1] == row["Normal"], 5] = row[args.t]
+
+    fam.to_csv(args.o, header=False, index=False, sep="\t")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", help="input phenotype file")
+    parser.add_argument("-f", help="input fam file")
+    parser.add_argument("-t", help="selected phenotype")
+    parser.add_argument("-o", help="output file")
+
+    main(parser.parse_args())
