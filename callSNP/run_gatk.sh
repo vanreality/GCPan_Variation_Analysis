bf9b2a6f0e968a18a0af5f36e40ae1dbfffa592c
fan <vanshiyu@gmail.com>
2021-07-05 18:05:36 +0800

    add map to pan gatk pipeline

---
+++ callSNP/run_gatk.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+script="/lustre/home/acct-clswcc/clswcc-fsy/gastricCancer/panGenome/script/callSNP"
+result="/lustre/home/acct-clswcc/clswcc-fsy/gastricCancer/panGenome/data/panGatkBams"
+
+#samples=$(cat 185samples)
+samples=$(head -n 1 185samples)
+mkdir $result
+cd $result || exit
+for sample in $samples;
+do
+  normal=$(echo $sample | awk -F "." '{print $1}')
+  tumor=$(echo $sample | awk -F "." '{print $2}')
+#  if [ -d $normal ] && [ -d $tumor ]; then
+#    continue
+#  fi
+  mkdir $normal
+  cd $normal || exit
+  cp $script/gatk.slurm ./
+  sbatch gatk.slurm $normal
+  cd ..
+
+  mkdir $tumor
+  cd $tumor || exit
+  cp $script/gatk.slurm ./
+  sbatch gatk.slurm $tumor
+  cd ..
+done
